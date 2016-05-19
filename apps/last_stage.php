9da92318ed19b97d71f5215448b0f3762e03908a
ahmad luky ramdani <luky.lucky@yahoo.com>
2016-05-19 08:57:05 +0700

    memperbaikit UI client

--- apps/last_stage.php
+++ apps/last_stage.php
@@ -3,14 +3,10 @@
 	$db = new SQLite3('../db/antrian.db');
 	$results = $db->query('SELECT Max(id) as id FROM data_antrian WHERE counter='.$loket.'');
 	$row = $results->fetchArray();
-<<<<<<< HEAD
 	if ($row['id'] == NULL) {
     	$data = array('next' => 0);
 	} else {
     	$data = array('next' => $row['id']);
 	}
-=======
-    $data = array('next' => $row['id']);
->>>>>>> 70f35c8a2fe26c4fea1c887f15f597731f08f4cb
     echo json_encode($data);
 ?>
